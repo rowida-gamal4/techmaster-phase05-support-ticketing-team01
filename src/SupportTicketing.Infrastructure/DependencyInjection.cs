a124f3bbf6765c53a0c8bf3db6fa2671a4153cc3
Sama Samy <samasamy68@gmail.com>
2026-08-20 20:51:29 +0300

    feat(infrastructure): configure EF Core database foundation

---
+++ src/SupportTicketing.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,19 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SupportTicketing.Infrastructure.Persistence;
+
+namespace SupportTicketing.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<AppDbContext>(options =>
+        {
+            options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+        });
+
+        return services;
+    }
+}
