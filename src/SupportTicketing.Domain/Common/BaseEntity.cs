8f3d54c7f4d14b1de34b8ab0f9c966cfc4d2a0b4
Rowida <rowidagamal33@gmail.com>
2026-08-22 20:47:14 +0300

    TICKET(S08):Adding identify tickets approaching sla target cqrs features

--- src/SupportTicketing.Domain/Common/BaseEntity.cs
+++ src/SupportTicketing.Domain/Common/BaseEntity.cs
@@ -2,9 +2,19 @@ namespace SupportTicketing.Domain.Common;
 
 public abstract class BaseEntity
 {
-    public int Id { get; protected set; }
+    public int Id { get; init; }
 
     public DateTime CreatedAt { get; protected set; }
 
     public DateTime UpdatedAt { get; protected set; }
+    public void SetCreatedAt()
+    {
+        CreatedAt = DateTime.UtcNow;
+        UpdatedAt = DateTime.UtcNow;
+    }
+
+    public void SetUpdatedAt()
+    {
+        UpdatedAt = DateTime.UtcNow;
+    }
 }
